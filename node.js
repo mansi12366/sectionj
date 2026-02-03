7eeaa66c2020105e1e4507488ddcba148cce47f3
Mansi Chauhan <mansirajput9045@gmail.com>
2026-02-03 09:28:56 +0530

    Removed node_modules and added gitignore

---
+++ node.js
@@ -0,0 +1,33 @@
+//   const fs = require('fs');
+//  fs.writeFile('example.txt','Hello,World!', function(e){  
+//        if(e) console.log("aaya mera error")
+//    console.log("File written successfully");    
+//  })
+
+//  const fs = require('fs');
+//  fs.appendFile('example.txt','Hello,World!', function(e){
+//      if(e) console.log("aaya mera error")
+//              console.log("File written successfully");    
+// })
+// const fs = require('fs');
+//  fs.copyFile('example.txt','mansi.txt', function(e){
+//      if(e) console.log("aaya mera error")
+//              console.log("File written successfully");    
+// })
+
+// const fs = require('fs');
+//  fs.rename('mansi.txt','prachi.txt', function(e){
+//      if(e) console.log("aaya mera error")
+//              console.log("File written successfully");    
+// })
+
+
+// const fs = require('fs');
+// fs.unlink('example.txt', function(e){
+//     if(e) console.log("aaya mera error")
+//     console.log("File written successfully");    
+// })
+
+
+
+
